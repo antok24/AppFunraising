8253ff99a0d884f2458d4a694f4b58d23bb79871
antok24 <antokbojonegoro@gmail.com>
2020-04-27 09:42:05 +0700

    title update

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,7 @@
 		<!-- Basic -->
 		<meta charset="UTF-8">
 
-		<title>Default Layout | Porto Admin - Responsive HTML5 Template 1.7.0</title>
+		<title>App Funraising</title>
 		<meta name="keywords" content="HTML5 Admin Template" />
 		<meta name="description" content="Porto Admin - Responsive HTML5 Template">
 		<meta name="author" content="okler.net">
